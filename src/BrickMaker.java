4a1c81a82bda978a28353e5a9bd4e6abe844b925
BT_4N2_11 <bruh@gmail.com>
2024-04-03 13:50:53 -0400

    added a BrickMaker class with its constructor using variables from LegoBrick and started working on CustomizeBrick method.

---
+++ src/BrickMaker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class BrickMaker extends LegoBrick{
+int size = 1;
+
+
+
+public BrickMaker(int size, String color){
+    super(size,color); }
+
+
+
+    public static void CustomizeBrick(int size, String color){
+    System.out.println("size or color:");
+    String brickCustomizeOption  = BrickMaker.nextLine();
+    if(brickCustomizeOption.equals("color")){
+
+    }
+if(brickCustomizeOption.equals(size)){
+
+    }
+}
+
+
+
+
+
