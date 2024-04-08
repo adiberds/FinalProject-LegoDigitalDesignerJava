4ac2d150ba4d30922ce78bd1c1c93042714267c0
BT_4N2_11 <adibm@nycstudents.net>
2024-04-08 13:44:17 -0400

    Fixed customizeBrick() method

--- src/BrickMaker.java
+++ src/BrickMaker.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
-public class BrickMaker extends LegoBrick{
-int size = 1;
+public class BrickMaker extends LegoBrick {
+    private int size = 1;
 
 
 
-public BrickMaker(int size, String color, String stud){
-    super(size,color, stud); }
+    public BrickMaker(int size, String color, String stud) {
+        super(size, color, stud);
+    }
 
 
-
-    public void CustomizeBrick(int size, String color, String stud){
+    public void customizeBrick(int size, String color, String stud) {
         Scanner scanner = new Scanner(System.in);
-    System.out.print("Click size or color to customize:");
+        System.out.print("Click size or color to customize:");
 
-    String brickCustomizeOption  = scanner.nextLine();
-    if(brickCustomizeOption.equalsIgnoreCase("color")) {
-        System.out.println("Enter new color:");
-        String newColor = scanner.nextLine();
-        LegoBrick.setColor(newColor);
-    }
-else if(brickCustomizeOption.equals("size")){
-        System.out.println("Enter new size:");
-        String newSize = scanner.nextLine();
-        LegoBrick.setSize(newSize);
-        System.out.println("Size Updated!")
-    }
-else BrickMaker.CustomizeBrick;
-
+        String brickCustomizeOption = scanner.nextLine();
+        if (brickCustomizeOption.equalsIgnoreCase("color")) {
+            System.out.println("Enter new color:");
+            String newColor = scanner.nextLine();
+            super.setColor(newColor);
+        } else if (brickCustomizeOption.equals("size")) {
+            System.out.println("Enter new size:");
+            String newSize = scanner.nextLine();
+            int newSizeInt = Integer.parseInt(newSize);
+            super.setSize(newSizeInt);
+            System.out.println("Size Updated!");
+        } else {
+            customizeBrick(size, color, stud);
+        }
     }
 }
 
