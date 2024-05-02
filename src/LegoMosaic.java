9a4fe44a03d57b4cd5464f24361419dda0e3e9ca
BT_4N2_11 <adibm@nycstudents.net>
2024-05-02 13:07:41 -0400

    May 1st commit (commit didn't go through on that day)
    used a hashmap instead of 2d array since I learned that you can contain value pairs, so I was able to input the object partsCount and LegoBrick as those values

--- src/LegoMosaic.java
+++ src/LegoMosaic.java
@@ -1,32 +1,21 @@
-public class LegoMosaic{
+import java.util.HashMap;
 
-    private LegoBrick[][] grid;
+public class LegoMosaic {
 
-    public LegoMosaic(int rows, int cols){
-        grid = new LegoBrick[rows][cols];
+    private HashMap<PartsCount, LegoBrick> grid;
+
+    public LegoMosaic(PartsCount, LegoBrick) {
+        grid = new LegoMosaic<PartsCount, LegoBrick>;
     }
     //making a object LegoBrick in the grid system
 
 
-public void addBrick(LegoBrick brick, int row, int col){
-    if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
-        grid[row][col] = brick;
+    public void addBrick(LegoBrick brick, int row, int col) {
+        grid.put(row + "," + col, brick);
     }
-    else { System.out.println("Cannot place brick!");
+
+
+    public void removeBrick(LegoBrick brick, int row, int col) {
+grid.remove(row + "," + col, brick);
     }
-}
-//addBrick which adds a brick in the grid when space is free, and when space isn't free, it does else which does a specific print statement
-
-    public void removeBrick(LegoBrick brick, int row, int col){
-        if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
-            grid[row][col] = null;
-        }
-        else { System.out.println("Cannot remove brick!");
-        }
-    } //removeBrick which removes a brick in the grid when needed, and for the else I did the same thing as above method
-    // however more code will be added for situations in which a brick above another brick wants to be kept however the brick under doesn't
-
-
-
-
 }
