5437f5fbbe5b0353215585b2b3274522b770282e
BT_4N2_11 <adibm@nycstudents.net>
2024-05-20 13:43:52 -0400

    -edited the mainframe as I am going to add a switch frame feature -added a new class

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 
 public class MainFrame extends JFrame {
+
 
 
     public MainFrame() {
@@ -11,6 +14,11 @@ public class MainFrame extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+
+
+
+
 
 
 
@@ -25,9 +33,13 @@ public class MainFrame extends JFrame {
         label.setFont(new Font("Papyrus", Font.BOLD, 30));
         backgroundLabel.add(label);
 
+        JPanel openingPanel = new JPanel ();
+        JPanel buildingPanel = new JPanel ();
 
-        JButton button = new JButton("Start Building");
+        JButton button = new JButton("Start Building 64 x 64");
         backgroundLabel.add(button);
+
+
 
 
 
@@ -35,7 +47,18 @@ public class MainFrame extends JFrame {
 
 
         setVisible(true);
+
     }
+    public void mouseClicked(MouseEvent e){
+if mouseClicked(){
+
+
+        }
+    }
+
+
+
+
 
 
     public static void main(String[] args) {
---
+++ src/SecondFrame64X64.java
@@ -0,0 +1,5 @@
+public class SecondFrame64X64 extends MainFrame{
+
+
+
+}
