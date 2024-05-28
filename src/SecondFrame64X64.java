f22b219d64b81119048ac774824b943b001a3f0e
BT_4N2_11 <adibm@nycstudents.net>
2024-05-28 13:44:06 -0400

    Figured how to make a grid of buttons using a for loop using link provided in journal however I need to learn how to organize it in a square!

--- src/SecondFrame64X64.java
+++ src/SecondFrame64X64.java
@@ -1,5 +1,29 @@
-public class SecondFrame64X64 extends MainFrame{
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.*;
 
 
+public class SecondFrame64X64{
 
+JFrame frame = new JFrame();
+JButton[][] LegoGrid;
+
+public SecondFrame64X64(int width, int length){
+    JFrame frame = new JFrame("LEGO Mosaic");
+    frame.setLayout(new GridLayout(length,width));
+    LegoGrid = new JButton[width][length];
+    for(int y = 0; y <length; y++){
+        for(int x = 0; x<width; x++){
+            LegoGrid[x][y] = new JButton();
+            frame.add(LegoGrid[x][y]);
+        }
+
+
+    }
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+}
+public static void main(String[] args){new SecondFrame64X64(100, 100);
+}
 }
