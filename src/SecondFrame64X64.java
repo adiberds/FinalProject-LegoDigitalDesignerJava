7ca2d19c51ec2c0a4b9fcc3e0fb1b52dc0f90c96
BT_4N2_11 <adibm@nycstudents.net>
2024-05-31 13:22:53 -0400

    used link in journal to edit SecondFrame

--- src/SecondFrame64X64.java
+++ src/SecondFrame64X64.java
@@ -5,15 +5,19 @@ import java.awt.*;
 
 public class SecondFrame64X64{
 
+
+    private static final int SIDE = 64;
+    private static final int BUTTON_WIDTH = 13;
+
 JFrame frame = new JFrame();
 JButton[][] LegoGrid;
 
 public SecondFrame64X64(int width, int length){
     JFrame frame = new JFrame("LEGO Mosaic");
-    frame.setLayout(new GridLayout(length,width));
+    frame.setLayout(new GridLayout(SIDE,SIDE, 1, 1));
     LegoGrid = new JButton[width][length];
-    for(int y = 0; y <length; y++){
-        for(int x = 0; x<width; x++){
+    for(int y = 0; y <SIDE; y++){
+        for(int x = 0; x<SIDE; x++){
             LegoGrid[x][y] = new JButton();
             frame.add(LegoGrid[x][y]);
         }
